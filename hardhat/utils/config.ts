02931eab6d7f10c221db1cace6618c6814833890
critesjosh <jc@joshcrites.com>
2023-11-05 21:40:37 -0500

    cleanup

--- hardhat/utils/config.ts
+++ hardhat/utils/config.ts
@@ -30,3 +30,13 @@ export const processTransferInputs = {
     C2y: 0x3006aa76f9499aeee9080237f3c24005be7ca83627f6600f7b278dff77a37df5n,
   },
 };
+
+export const account2 =
+  "0x0c07999c15d406bc08d7f3f31f62cedbc89ebf3a53ff4d3bf7e2d0dda9314904" as `0x${string}`;
+export const account1 =
+  "0xdc9f9fdb746d0f07b004cc4316e3495a58570b90661499f8a6a6696ff4156baa" as `0x${string}`;
+export const processFeeRecipient =
+  "0xbEa2940f35737EDb9a9Ad2bB938A955F9b7892e3" as `0x${string}`;
+
+export const BJJ_PRIME =
+  21888242871839275222246405745257275088548364400416034343698204186575808495617n;
