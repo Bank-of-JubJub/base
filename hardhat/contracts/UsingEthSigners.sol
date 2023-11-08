cdb83c1db88c8d62dbda2ba26d1854a7a6dd6d36
critesjosh <jc@joshcrites.com>
2023-11-08 06:13:14 -0500

    add public inputs

--- hardhat/contracts/UsingEthSigners.sol
+++ hardhat/contracts/UsingEthSigners.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 // import {UltraVerifier as } from "./withdraw/plonk_vk.sol";
-// import {UltraVerifier as LockVerifier} from "./lock/plonk_vk.sol";
 
 contract UsingEthSingers {
     struct MultisigParams {
@@ -14,72 +13,56 @@ contract UsingEthSingers {
     mapping(bytes32 packedPublicKey => MultisigParams signers)
         public multisigEthSigners;
     mapping(bytes32 packedPublicKey => uint256 nonce) public nonce;
-
+    uint256 BJJ_PRIME =
+        21888242871839275222246405745257275088548364400416034343698204186575808495617;
     // Verifiers
+    // change can be used to revoke, just set to address(0)
     address public addEthSignerVerifier;
-    address public revokeEthSignerVerifier;
     address public changeEthSignerVerifier;
     address public addMultisigEthSignerVerifier;
-    address public revokeMultisigEthSignerVerifier;
     address public changeMultisigEthSignerVerifier;
 
     constructor(
         address _addEthSignerVerifier,
-        address _revokeEthSignerVerifier,
         address _changeEthSignerVerfier,
         address _addMultisigEthSignerVerifier,
-        address _revokeMultisigEthSignerVerifier,
         address _changeMultisigEthSignerVerifier
     ) {
         addEthSignerVerifier = _addEthSignerVerifier;
-        revokeEthSignerVerifier = _revokeEthSignerVerifier;
         changeEthSignerVerifier = _changeEthSignerVerfier;
         addMultisigEthSignerVerifier = _addMultisigEthSignerVerifier;
-        revokeMultisigEthSignerVerifier = _revokeMultisigEthSignerVerifier;
         changeMultisigEthSignerVerifier = _changeMultisigEthSignerVerifier;
     }
 
     function addEthSigner(
-        bytes32 packedPublicKey,
-        address ethSignerAddress
+        bytes32 _packedPublicKey,
+        address _ethSignerAddress
     ) public {
         require(
-            ethSigner[packedPublicKey] == address(0x0),
+            ethSigner[_packedPublicKey] == address(0x0),
             "eth signer already exists"
         );
-        // public inputs
-        // packed public key
-        // eth signer address
-        // nonce
+
+        bytes32[] memory publicInputs = new bytes32[](2);
+        // % p, so it fits in a field element
+        // must pass full packed key as private input to circuit and check the hash matches
+        publicInputs[0] = bytes32(uint256(_packedPublicKey) % BJJ_PRIME);
+        // message hash, signed by eth account
+        // it should be unique and not reusable
+        publicInputs[1] = keccak256(
+            abi.encodePacked(
+                address(this),
+                _packedPublicKey,
+                _ethSignerAddress,
+                nonce[_packedPublicKey]
+            )
+        );
 
         // The proof checks that the caller has the private key corresponding to the public key
         // addEthSignerVerifier.verify(proof, publicInputs);
 
-        nonce[packedPublicKey] += 1;
-        ethSigner[packedPublicKey] = ethSignerAddress;
-    }
-
-    function revokeEthSigner(
-        bytes memory _proof,
-        bytes32 packedPublicKey
-    ) public {
-        require(
-            ethSigner[packedPublicKey] != address(0x0),
-            "eth signer does not exist"
-        );
-        address signer = ethSigner[packedPublicKey];
-        // public input
-        // signer address
-        // messageHash. the signed message should include the contract address, the packed public key and the nonce
-        bytes32 messageHash = getMessageHash(
-            packedPublicKey,
-            nonce[packedPublicKey]
-        );
-        // the circuit must check that signature (private inputs) is a valid signature of the messageHash
-        // and comes from the signer
-        // revokeEthSignerVerifier.verify(proof, publicInputs);
-        nonce[packedPublicKey] += 1;
-        delete ethSigner[packedPublicKey];
+        nonce[_packedPublicKey] += 1;
+        ethSigner[_packedPublicKey] = _ethSignerAddress;
     }
 
     function changeEthSigner(
@@ -93,16 +76,22 @@ contract UsingEthSingers {
         );
         address signer = ethSigner[_packedPublicKey];
 
-        // public inputs
-        // signer address
-        bytes32 messageHash = getMessageHash(
-            _packedPublicKey,
-            nonce[_packedPublicKey]
+        bytes32[] memory publicInputs = new bytes32[](2);
+        // recover this address in the circuit
+        publicInputs[0] = bytes32(uint256(uint160(signer)));
+        // recover with this message hash, it should be unique and not reusable
+        publicInputs[1] = keccak256(
+            abi.encodePacked(
+                address(this),
+                _packedPublicKey,
+                _newEthSignerAddress,
+                nonce[_packedPublicKey]
+            )
         );
 
         // the circuit must check that the caller has the private key corresponding to the public key
         // and that the signature is a valid signature of the messageHash and comes from the current
-        // changeEthSignerVerifier.verify(proof, publicInputs);
+        // changeEthSignerVerifier.verify(_proof, publicInputs);
         nonce[_packedPublicKey] += 1;
         ethSigner[_packedPublicKey] = _newEthSignerAddress;
     }
@@ -128,54 +117,54 @@ contract UsingEthSingers {
         );
 
         // public inputs
-        // packed public key
-        // eth signer addresses array
-        // nonce
+        bytes32[] memory publicInputs = new bytes32[](2);
+        // private key corresponding to the packed public key controls the account until registered
+        publicInputs[0] = bytes32(uint256(_packedPublicKey) % BJJ_PRIME);
+        // this should be unique and not reusable
+        publicInputs[1] = keccak256(
+            abi.encodePacked(
+                address(this),
+                _packedPublicKey,
+                _ethSignerAddresses,
+                _threshold,
+                nonce[_packedPublicKey]
+            )
+        );
+
         // the circuit must check that the caller has the private key corresponding to the public key
         // addMultisigEthSignerVerifier.verify(proof, publicInputs);
         nonce[_packedPublicKey] += 1;
         multisigEthSigners[_packedPublicKey].ethSigners = _ethSignerAddresses;
     }
 
-    function revokeMultisigEthSigners(
-        bytes memory _proof,
-        bytes32 _packedPublicKey,
-        address[] memory _ethSignerAddresses
-    ) public {
-        require(
-            multisigEthSigners[_packedPublicKey].ethSigners.length > 0,
-            "eth signer must exist"
-        );
-        // public inputs
-        // _ethSignerAddresses
-        // signer threshold
-        bytes32 messageHash = getMessageHash(
-            _packedPublicKey,
-            nonce[_packedPublicKey]
-        );
-
-        // the circuit must check that signatures (private inputs) are valid signature of the messageHash
-        // and comes from the signers and meets the signer threshold
-        // revokeMultisigEthSignerVerifier.verify(proof, publicInputs);
-        nonce[_packedPublicKey] += 1;
-        delete multisigEthSigners[_packedPublicKey];
-    }
-
     function changeMultisigEthSigners(
         bytes memory _proof,
         bytes32 _packedPublicKey,
-        address[] memory _newEthSignerAddresses
+        address[] memory _newEthSignerAddresses,
+        uint256 _threshold
     ) public {
         require(
             multisigEthSigners[_packedPublicKey].ethSigners.length > 0,
             "eth signer must exist"
         );
         // public inputs
-        // _newEthSignerAddresses
-        // signer threshold
-        bytes32 messageHash = getMessageHash(
-            _packedPublicKey,
-            nonce[_packedPublicKey]
+        bytes32[] memory publicInputs = new bytes32[](11);
+        // hardcoded max of 10 signers, this can be increased
+        for (uint8 i; i <= 10; i++) {
+            publicInputs[i] = bytes32(
+                uint256(
+                    uint160(multisigEthSigners[_packedPublicKey].ethSigners[i])
+                )
+            );
+        }
+        publicInputs[11] = keccak256(
+            abi.encodePacked(
+                address(this),
+                _packedPublicKey,
+                _newEthSignerAddresses,
+                _threshold,
+                nonce[_packedPublicKey]
+            )
         );
 
         // the circuit must check that signatures (private inputs) are valid signatures of the messageHash
@@ -185,15 +174,5 @@ contract UsingEthSingers {
         nonce[_packedPublicKey] += 1;
         multisigEthSigners[_packedPublicKey]
             .ethSigners = _newEthSignerAddresses;
-    }
-
-    function getMessageHash(
-        bytes32 _packedPublicKey,
-        uint256 _nonce
-    ) internal returns (bytes32) {
-        return
-            keccak256(
-                abi.encodePacked(address(this), _packedPublicKey, _nonce)
-            );
     }
 }
