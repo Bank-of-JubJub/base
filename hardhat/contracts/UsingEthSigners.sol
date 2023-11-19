a8be0fbb458cf447cb00aa0a028d7324ceed81f7
critesjosh <jc@joshcrites.com>
2023-11-19 11:02:25 -0500

    fix no mod

--- hardhat/contracts/UsingEthSigners.sol
+++ hardhat/contracts/UsingEthSigners.sol
@@ -48,7 +48,7 @@ contract UsingEthSingers {
             publicInputs[i] = bytes32(uint256(uint8(aByte)));
         }
         // The nonce ensures the proof cannot be reused
-        publicInputs[1] = bytes32(nonce[_packedPublicKey]);
+        publicInputs[32] = bytes32(nonce[_packedPublicKey]);
 
         // The proof checks that the caller has the private key corresponding to the public key
         // addEthSignerVerifier.verify(proof, publicInputs);
@@ -68,11 +68,7 @@ contract UsingEthSingers {
         );
         address signer = ethSigner[_packedPublicKey];
 
-        bytes32[] memory publicInputs = new bytes32[](2);
-        // recover this address in the circuit
-        publicInputs[0] = bytes32(uint256(uint160(signer)));
-        // recover with this message hash, it should be unique and not reusable
-        publicInputs[1] = keccak256(
+        bytes32 messageHash = keccak256(
             abi.encodePacked(
                 address(this),
                 _packedPublicKey,
@@ -80,6 +76,17 @@ contract UsingEthSingers {
                 nonce[_packedPublicKey]
             )
         );
+
+        bytes32[] memory publicInputs = new bytes32[](32);
+        // recover this address in the circuit
+        publicInputs[0] = bytes32(uint256(uint160(signer)));
+        // recover address with signature over this message hash
+        // it should be unique and not reusable
+        for (uint8 i = 0; i < 32; i++) {
+            // Noir takes an array of 32 bytes32 as public inputs
+            bytes1 aByte = bytes1((messageHash << (i * 8)));
+            publicInputs[i] = bytes32(uint256(uint8(aByte)));
+        }
 
         // the circuit must check that the caller has the private key corresponding to the public key
         // and that the signature is a valid signature of the messageHash and comes from the current
@@ -109,11 +116,15 @@ contract UsingEthSingers {
         );
 
         // public inputs
-        bytes32[] memory publicInputs = new bytes32[](2);
+        bytes32[] memory publicInputs = new bytes32[](32);
         // private key corresponding to the packed public key controls the account until registered
-        publicInputs[0] = bytes32(uint256(_packedPublicKey) % BJJ_PRIME);
+        for (uint8 i = 0; i < 32; i++) {
+            // Noir takes an array of 32 bytes32 as public inputs
+            bytes1 aByte = bytes1((_packedPublicKey << (i * 8)));
+            publicInputs[i] = bytes32(uint256(uint8(aByte)));
+        }
         // this should be unique and not reusable
-        publicInputs[1] = bytes32(nonce[_packedPublicKey]);
+        publicInputs[32] = bytes32(nonce[_packedPublicKey]);
 
         // the circuit must check that the caller has the private key corresponding to the public key
         // can use the same circuit as single addEthSigner
@@ -132,17 +143,8 @@ contract UsingEthSingers {
             multisigEthSigners[_packedPublicKey].ethSigners.length > 0,
             "eth signer must exist"
         );
-        // public inputs
-        bytes32[] memory publicInputs = new bytes32[](11);
-        // hardcoded max of 10 signers, this can be increased
-        for (uint8 i; i <= 10; i++) {
-            publicInputs[i] = bytes32(
-                uint256(
-                    uint160(multisigEthSigners[_packedPublicKey].ethSigners[i])
-                )
-            );
-        }
-        publicInputs[11] = keccak256(
+
+        bytes32 messageHash = keccak256(
             abi.encodePacked(
                 address(this),
                 _packedPublicKey,
@@ -151,6 +153,21 @@ contract UsingEthSingers {
                 nonce[_packedPublicKey]
             )
         );
+        // public inputs
+        bytes32[] memory publicInputs = new bytes32[](42);
+        // hardcoded max of 10 signers, this can be increased
+        for (uint8 i; i <= 10; i++) {
+            publicInputs[i] = bytes32(
+                uint256(
+                    uint160(multisigEthSigners[_packedPublicKey].ethSigners[i])
+                )
+            );
+        }
+        for (uint8 i = 0; i < 32; i++) {
+            // Noir takes an array of 32 bytes32 as public inputs
+            bytes1 aByte = bytes1((messageHash << (i * 8)));
+            publicInputs[i + 10] = bytes32(uint256(uint8(aByte)));
+        }
 
         // the circuit must check that signatures (private inputs) are valid signatures of the messageHash
         // and come from the current list of signers and meet the signer threshold
