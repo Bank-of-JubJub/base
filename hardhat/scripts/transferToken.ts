0fe2806c9c096f6c75e8f4e98896be7777966544
critesjosh <jc@joshcrites.com>
2023-12-24 21:44:18 -0500

    working

--- hardhat/scripts/transferToken.ts
+++ hardhat/scripts/transferToken.ts
@@ -6,10 +6,11 @@ import { BojAccount } from "../utils/types";
 import { hexToBigInt, toBytes, toHex } from "viem";
 import { TransferCoordinator } from "../coordinators/TransferCoordinator";
 import BabyJubJubUtils from "../utils/babyJubJubUtils";
+import { account2 } from "../utils/constants";
 dotenv.config({ path: "../.env" });
 
 const params = {
-  to: "0xdc9f9fdb746d0f07b004cc4316e3495a58570b90661499f8a6a6696ff4156baa" as `0x${string}`,
+  to: account2.packedPublicKey,
   amount: 2,
   processFee: 1,
   relayFee: 1,
@@ -20,13 +21,6 @@ async function main() {
   await babyjub.init();
   const publicClient = await hre.viem.getPublicClient();
   const [sender] = await hre.viem.getWalletClients();
-  const { data: privateTokenData } = readDeploymentData("PrivateToken");
-  const network = hre.network.name;
-
-  let privateToken = await hre.viem.getContractAt(
-    "PrivateToken",
-    privateTokenData[network].address
-  );
 
   const pubKey = babyjub.privateToPublicKey(
     hexToBigInt(process.env.BOJ_PRIVATE_KEY as `0x${string}`)
