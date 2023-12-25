21ef536bb5a8c7902152bbc73425078b03416438
Josh Crites <jc@joshcrites.com>
2023-12-25 12:07:11 -0500

    add withdraw script

---
+++ hardhat/scripts/withdraw.ts
@@ -0,0 +1,55 @@
+import hre from "hardhat";
+import dotenv from "dotenv";
+import { delay } from "../utils/utils";
+import { BojAccount } from "../utils/types";
+import { hexToBigInt, toBytes, toHex } from "viem";
+import BabyJubJubUtils from "../utils/babyJubJubUtils";
+import { WithdrawCoordinator } from "../coordinators/WithdrawCoordinator";
+dotenv.config({ path: "../.env" });
+
+const params = {
+    amount: 10,
+    relayFee: 0,
+    relayFeeRecipent: "0x7D678b9218aC289e0C9F18c82F546c988BfE3022" as `0x${string}`
+};
+const babyjub = new BabyJubJubUtils();
+
+async function main() {
+    await babyjub.init();
+
+    const publicClient = await hre.viem.getPublicClient();
+    const [sender] = await hre.viem.getWalletClients();
+
+    const pubKey = babyjub.privateToPublicKey(
+        hexToBigInt(process.env.BOJ_PRIVATE_KEY as `0x${string}`)
+    );
+
+    const bojAccount = {
+        packedPublicKey: toHex(
+            babyjub.packPublicKey([toBytes(pubKey.x), toBytes(pubKey.y)])
+        ),
+        privateKey: process.env.BOJ_PRIVATE_KEY,
+    } as BojAccount;
+
+    const withdrawCoordinator = new WithdrawCoordinator(
+        sender.account.address,
+        bojAccount,
+        params.amount,
+        params.relayFee,
+        params.relayFeeRecipent
+    );
+
+    await withdrawCoordinator.init();
+    await withdrawCoordinator.generateProof();
+    const hash = await withdrawCoordinator.sendWithdraw();
+
+    await delay(5000);
+
+    const receipt = await publicClient.getTransactionReceipt({ hash });
+    console.log(receipt);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
