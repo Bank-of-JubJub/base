33872b769b26d23135a014b0c528fa5838c8f28c
Josh Crites <jc@joshcrites.com>
2024-01-01 21:53:35 -0500

    comment out

--- hardhat/contracts/PrivateTokenFactory.sol
+++ hardhat/contracts/PrivateTokenFactory.sol
@@ -1,53 +1,53 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.20;
 
-import {PrivateToken} from "./PrivateToken.sol";
-import {IERC20} from "./IERC20.sol";
+// import {PrivateToken} from "./PrivateToken.sol";
+// import {IERC20} from "./IERC20.sol";
 
-contract PrivateTokenFactory {
-    address public processDepositVerifier;
-    address public processTransferVerifier;
-    address public transferVerifier;
-    address public withdrawVerifier;
-    address public lockVerifier;
-    address public accountController;
-    address public transferVerifyLib;
-    address public poseidon;
+// contract PrivateTokenFactory {
+//     address public processDepositVerifier;
+//     address public processTransferVerifier;
+//     address public transferVerifier;
+//     address public withdrawVerifier;
+//     address public lockVerifier;
+//     address public accountController;
+//     address public transferVerifyLib;
+//     address public poseidon;
 
-    event Deployed(address indexed token);
+//     event Deployed(address indexed token);
 
-    // TODO: update to use the the all transfer and all withdraw verifiers
-    constructor(
-        address _pendingDepositVerifier,
-        address _pendingTransferVerifier,
-        address _transferVerifier,
-        address _withdrawVerifier,
-        address _lockVerifier,
-        address _accountController,
-        address _transferVerifyLib,
-        address _poseidon
-    ) {
-        processDepositVerifier = _pendingDepositVerifier;
-        processTransferVerifier = _pendingTransferVerifier;
-        transferVerifier = _transferVerifier;
-        withdrawVerifier = _withdrawVerifier;
-        lockVerifier = _lockVerifier;
-        accountController = _accountController;
-        transferVerifyLib = _transferVerifyLib;
-        poseidon = _poseidon;
-    }
+//     // TODO: update to use the the all transfer and all withdraw verifiers
+//     constructor(
+//         address _pendingDepositVerifier,
+//         address _pendingTransferVerifier,
+//         address _transferVerifier,
+//         address _withdrawVerifier,
+//         address _lockVerifier,
+//         address _accountController,
+//         address _transferVerifyLib,
+//         address _poseidon
+//     ) {
+//         processDepositVerifier = _pendingDepositVerifier;
+//         processTransferVerifier = _pendingTransferVerifier;
+//         transferVerifier = _transferVerifier;
+//         withdrawVerifier = _withdrawVerifier;
+//         lockVerifier = _lockVerifier;
+//         accountController = _accountController;
+//         transferVerifyLib = _transferVerifyLib;
+//         poseidon = _poseidon;
+//     }
 
-    function deploy(address _token) public {
-        PrivateToken newToken = new PrivateToken(
-            processDepositVerifier,
-            processTransferVerifier,
-            transferVerifier,
-            withdrawVerifier,
-            lockVerifier,
-            _token,
-            18,
-            poseidon
-        );
-        emit Deployed(address(newToken));
-    }
-}
+//     function deploy(address _token) public {
+//         PrivateToken newToken = new PrivateToken(
+//             processDepositVerifier,
+//             processTransferVerifier,
+//             transferVerifier,
+//             withdrawVerifier,
+//             lockVerifier,
+//             _token,
+//             18,
+//             poseidon
+//         );
+//         emit Deployed(address(newToken));
+//     }
+// }
