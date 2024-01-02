489179cfb2b8e3bd9b40438c7397f7d7cbb2203b
Josh Crites <jc@joshcrites.com>
2024-01-02 13:23:23 -0500

    rename

--- hardhat/scripts/buildMerkleTree.ts
+++ hardhat/scripts/buildMerkleTree.ts
@@ -3,12 +3,16 @@ dotenv.config({ path: "../.env" });
 import { IMT } from "@zk-kit/imt";
 import { poseidon2, poseidon3 } from "poseidon-lite";
 import { toHex } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
 
 async function main() {
   const secret =
     168986485046885582825082387270879151100288537211746581237924789162159767777n;
-  const amount = 5n;
-  const commitment = poseidon2([secret, amount]);
+  const amount = 10n;
+  const account = privateKeyToAccount(process.env.PRIVATE_KEY as `0x${string}`);
+  const commitment = poseidon3([account.address, amount, secret]);
+
+  console.log(account.address);
 
   const tree = new IMT(poseidon2, 20, 0);
   tree.insert(commitment);
